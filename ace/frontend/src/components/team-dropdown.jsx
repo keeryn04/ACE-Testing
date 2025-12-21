4c13ca7b2afef894b4b2c7ff77aa6951999296c8
keeryn04 <keerynjohnson@gmail.com>
2025-12-21 16:36:52 -0700

    Update team-dropdown.jsx

--- ace/frontend/src/components/team-dropdown.jsx
+++ ace/frontend/src/components/team-dropdown.jsx
@@ -2,44 +2,31 @@ import React, { useEffect, useState } from "react";
 const API_BASE = import.meta.env.VITE_API_BASE;
 
 export default function TeamDropdown() {
-  const [userTeam, setUserTeam] = useState<{ name: String } | null>(null);
+  const [userTeam, setUserTeam] = useState(null);
   const [loading, setLoading] = useState(true);
 
   const fetchUserAndTeams = async () => {
     const token = localStorage.getItem("token");
-    if (!token) {
-      setLoading(false);
-      return;
-    }
+    if (!token) return setLoading(false);
 
     try {
-      // Decode token
+      //decode token
       const res = await fetch(`${API_BASE}/api/decode_token`, {
         headers: { Authorization: `Bearer ${token}` },
       });
-      if (!res.ok) {
-        setUserTeam(null);
-        return;
-      }
+      if (!res.ok) return null;
 
       const decoded = await res.json();
       const currentUserId = decoded.current_user;
 
-      // Fetch user's team
+      //fetch user's team
       const userRes = await fetch(`${API_BASE}/api/team/${currentUserId}`, {
         headers: { Authorization: `Bearer ${token}` },
       });
-
-      if (!userRes.ok) {
-        setUserTeam(null);
-        return;
-      }
-
       const userData = await userRes.json();
-      setUserTeam(userData?.team || null);
+      setUserTeam(userData.team || null);
     } catch (err) {
       console.error(err);
-      setUserTeam(null);
     } finally {
       setLoading(false);
     }
